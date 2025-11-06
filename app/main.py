985f8ff18c30b281706e301e855dcb6d973b5616
Jaison Schmidt <31486070+jaisonschmidt@users.noreply.github.com>
2025-11-06 00:11:39 +0000

    Adiciona modelo e atualiza dependencias

--- app/main.py
+++ app/main.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def hello_world():
     return jsonify({"message": "Hello World, Flask API is running!"})
 
+@app.route('/health-check', methods=['GET'])
+def health():
+    return jsonify({"status": "OK"})
+
+@app.route('/user/<name>', methods=['GET'])
+def get_user(name):
+    return jsonify({"message": f"Olá, {name}! Seja bem-vindo à API Flask."})
+
+@app.route('/echo', methods=['POST'])
+def echo_data():
+    data = request.get_json()  # lê o corpo da requisição em JSON
+    return jsonify({
+        "received_data": data,
+        "message": "Dados recebidos com sucesso!"
+    })
+
 if __name__ == '__main__':
     app.run(debug=True)
